a5b01722a6ff1b9ef8212cf66530d67913679f5c
blakbox23 <boudii30@gmail.com>
2021-04-20 17:06:31 +0300

    Bubble_sort Solution

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,15 +1,15 @@
 def bubble_sort(arr)
-    loop do
-        swapped=false;
-        (arr.length-1).times  do |i|
-            if arr[i]>arr[i+1] 
-            arr[i],arr[i+1]=arr[i+1], arr[i]
-            swapped =true;
-            end
-        end
-      break if swapped==false;
+  loop do
+    swapped = false
+    (arr.length - 1).times do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
     end
-    return arr
+    break if swapped == false
+  end
+  arr
 end
 
-puts bubble_sort([9,7,3,8,1])
+puts bubble_sort([9, 7, 3, 8, 1])
