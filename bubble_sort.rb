988eef595b21765d4c190813c0bc4a0994b24679
Barack <barackmukelenga100@gmail.com>
2021-04-20 16:26:48 +0200

    Solution for the bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,3 +13,26 @@ def bubble_sort(arr)
 end
 
 puts bubble_sort([9, 7, 3, 8, 1])
+
+
+# bubble_sort_by
+
+def bubble_sort_by(arr)
+  loop do
+    swapped = false
+    (arr.length - 1).times do |i|
+      if (yield arr[i], arr[i+1]) > 0 
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
+    end
+    break if swapped == false
+  end
+  arr
+end
+
+bubble_sort_in_action = bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
+
+puts bubble_sort_in_action
